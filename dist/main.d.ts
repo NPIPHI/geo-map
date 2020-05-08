6cfb12cfeb5b81c7298f12d8a33e51bb4a1304bd
Nicholas Phillips <nwp2@illinois.edu>
2020-05-08 18:47:11 -0500

    removed demo file from build

--- dist/main.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function invalidate(): void;
-export declare function incrementFeatureNumberDisplay(featureNumberDelta: number): void;
-export declare function setHoveredElement(id: string): void;
